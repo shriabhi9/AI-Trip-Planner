019c3a8bf263ede467215f100a92a434b4e408e1
abhishek <shriabhishek929@gmail.com>
2024-09-21 12:21:00 +0530

    Setup complete

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        setup complete
+      </div>
+    </>
+  );
+}
+
+export default App;
